41badca57b93df760b7de587528ad98e0bdd778b
PradeepBiligiri <bhadripradeepbiligiri@gmail.com>
2023-01-23 06:19:01 +0530

    navbar

---
+++ src/NavBar.js
@@ -0,0 +1,13 @@
+import { AppBar, Toolbar, Typography } from "@mui/material/";
+
+export function NavBar() {
+  return (
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography>Admin portal</Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+}
